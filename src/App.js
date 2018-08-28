c699f44ae39051adffefae36f15d462dfb662a80
Divyanshu Maithani <div.blackcat@gmail.com>
2018-08-28 19:49:48 +0530

    :construction: Init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+class App extends Component {
+    render() {
+        return <Button shape="circle" icon="search" />;
+    }
+}
+
+export default App;
