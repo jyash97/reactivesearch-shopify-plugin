1aaf6c981d690283107f37c7683e68ee792fc4e5
kuldeep <kuldepsaxena155@gmail.com>
2018-12-06 00:13:39 +0530

    feat(): Add support for custom id

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import ProductSuggestions from './components/ProductSuggestions';
 import './index.css';
 
 const mode = window.MODE;
+const id = window.REACTIVE_SEARCH_SHOPIFY_ID;
 
 if (mode === 'product-recommendations') {
     ReactDOM.render(
         <ProductSuggestions />,
         document.getElementById(
-            'reactivesearch-shopify-product-recommendations',
+            id || 'reactivesearch-shopify-product-recommendations',
         ),
     );
 } else {
     ReactDOM.render(
         <SearchPlugin />,
-        document.getElementById('reactivesearch-shopify'),
+        document.getElementById(id || 'reactivesearch-shopify'),
     );
 }
