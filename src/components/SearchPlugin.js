4f56f66eb73e89420de50b4243290f779c0ba98d
kuldeep <kuldepsaxena155@gmail.com>
2019-06-18 14:35:57 +0530

    fix: Minor fixes

--- src/components/SearchPlugin.js
+++ src/components/SearchPlugin.js
@@ -3,8 +3,8 @@ import { css } from 'react-emotion';
 import { Button, Modal, Icon } from 'antd';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { getPreferences } from '../utils';
 import get from 'lodash.get';
+import { getPreferences } from '../utils';
 
 import Search from './Search';
 
